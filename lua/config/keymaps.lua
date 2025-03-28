37c3334e3cdbd09cf6fe2be8610d36047d11e190
devon66h <devon66h@gmail.com>
2025-03-28 14:04:03 -0700

    Add keymap for toggling NvimTree, as well as show hidden files for ff and lg keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,5 +6,7 @@ vim.keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }
 vim.keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) 
 vim.keymap.set("n", "<leader>sx", ":close<CR>", { desc = "Close current split window" }) 
 
-vim.keymap.set("n", "<leader>ff", ":Telescope find_files<CR>", { desc = "Find files" }) 
-vim.keymap.set("n", "<leader>lg", ":Telescope live_grep<CR>", { desc = "Live grep" }) 
+vim.keymap.set("n", "<leader>ff", ":Telescope find_files hidden=true<CR>", { desc = "Find files" }) 
+vim.keymap.set("n", "<leader>lg", ":Telescope live_grep hidden=true<CR>", { desc = "Live grep" })
+
+vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { desc = "Toggle NvimTree" })
